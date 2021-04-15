6116c3e42537b8953ca161c3ca27903ed8c67db7
colby-reyes <colby.n.reyes@gmail.com>
2021-04-14 17:04:23 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# system management packages
+import sys 
+import os
+import shutil
+import math
+import datetime
+
+# plots
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+# image operation
+import cv2
+import moviepy.editor as mp
+
+# special packages
+import pytube
+import re

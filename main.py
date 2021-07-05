eac60932ba6a3fb07c73b92e7fe0ea4b3f8af035
colby-reyes <colby.n.reyes@gmail.com>
2021-07-05 12:56:06 -0700

    add files

--- main.py
+++ main.py
@@ -14,5 +14,27 @@ import cv2
 import moviepy.editor as mp
 
 # special packages
-import pytube
+from pytube import YouTube
+import requests
 import re
+
+video_url = 'https://www.youtube.com/watch?v=YWhSQpUNGgY'
+# video.streams.filter(file_extension = "mp4").all()
+youtube = YouTube(video_url)
+video = youtube.streams.first()
+video.download(filename="Sticks_and_Stones")
+print(video.filesize)
+
+
+# file_url = "https://www.youtube.com/watch?v=YWhSQpUNGgY"
+# file_name = "Sticks and Stone.mp4"
+
+# r = requests.get(file_url, stream = True)
+
+# # download started 
+# with open(file_name, 'wb') as f: 
+#     for chunk in r.iter_content(chunk_size = 1024*1024): 
+#         if chunk: 
+#             f.write(chunk) 
+  
+# print(f"{file_name} downloaded!\n")
